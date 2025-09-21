d5c1e2dd1a62c0761e80de4b0730ef9149da03e1
subbadevsecops <subbareddyn1992@gmail.com>
2025-09-21 15:27:45 +0530

    variable-file

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=subba
+PERSON2=mani
+
+echo "$PERSON1 :: Hi $PERSON2, hor are you"
+echo "$PERSON2 :: Hi $PERSON1,Good areyou"
+echo "$PERSON1 :: please provide the update of $PERSON2"
+echo "$PERSON2 :: please check update from $PERSON1"
